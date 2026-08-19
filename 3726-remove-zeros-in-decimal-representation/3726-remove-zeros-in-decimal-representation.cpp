5636be6e2aca3bf6d2aa2ca6f827fea7aef615b2
Karan Kavthankar <kgksleetcode@gmail.com>
2026-08-19 21:31:15 +0530

    Time: 0 ms (100%), Space: 9.2 MB (41.99%) - LeetHub

--- 3726-remove-zeros-in-decimal-representation/3726-remove-zeros-in-decimal-representation.cpp
+++ 3726-remove-zeros-in-decimal-representation/3726-remove-zeros-in-decimal-representation.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
     long long removeZeros(long long n) {
-        long long ans=0;
-        vector<int> arr;
-        while(n>0){
-            int digit=n%10;
-            if(digit==0){
-                n/=10;
-                continue;
+        string s=to_string(n);
+        int j=0;
+
+        for(int i=0;i<s.size();i++){
+            if(s[i]!='0'){
+                s[j++]=s[i];
             }
-            arr.push_back(digit);
-            n/=10;
-        }
-        ans=arr[arr.size()-1];
-
-        if(arr.size()==1){
-            return ans;
         }
 
-        for(int i=arr.size()-2;i>=0;i--){
-            ans=ans*10+arr[i];
-        }
-        return ans;
+        s.resize(j);
+        return stol(s);
     }
 };
